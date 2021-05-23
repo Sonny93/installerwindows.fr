691579cdc0c57d49dfd50623dd4c4992862cb370
Sonny <sonnyasdev@gmail.com>
2021-05-23 20:09:21 +0200

    Ajout rel "noreferrer" sur les liens externes

--- src/Components/NeedHelp/index.jsx
+++ src/Components/NeedHelp/index.jsx
@@ -5,7 +5,7 @@ import './ineedhelp.css';
 
 export default function NeedHelp({ }) {
     return <>
-        <a href="https://discord.gg/informatique" className="ineedhelp" target="_blank">
+        <a href="https://discord.gg/informatique" rel="noreferrer" className="ineedhelp" target="_blank">
             <svg width="165px" height="50px" viewBox="0 0 165 50" className="border">
                 <polyline points="164,1 164,49 1,49 1,1 164,1" className="bg-line" />
                 <polyline points="164,1 164,49 1,49 1,1 164,1" className="hl-line" />
