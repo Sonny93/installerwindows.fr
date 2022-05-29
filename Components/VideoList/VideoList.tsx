dd2ca1ffc95e83f81b915b3db5ab9b505faddb59
Sonny <sonny.lepapa@gmail.com>
2022-05-29 19:39:22 +0200

    fix: hydratation failed
    Error caused by react-responsive lib

--- Components/VideoList/VideoList.tsx
+++ Components/VideoList/VideoList.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import MediaQuery from 'react-responsive';
+import React, { useState } from 'react';
 
 import Carousel, { ResponsiveType } from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
@@ -8,7 +7,6 @@ import styles from '../../styles/videos.module.scss';
 import VideoItem from './VideoItem';
 
 export default function VideoList({
-    queries,
     carouselQueries,
     videos,
     currentVideo,
@@ -16,7 +14,6 @@ export default function VideoList({
     carouselRef,
     handleChangeVideo
 }: {
-    queries: { minWidth: number; maxWidth: number; }
     carouselQueries: ResponsiveType;
     videos: Video[];
     currentVideo: Video;
@@ -25,12 +22,11 @@ export default function VideoList({
     handleChangeVideo: (currentVideoId: string, videos: Video[], direction: 'next' | 'keep' | 'previous') => void;
 }) {
     const [showModal, setShowModal] = useState<boolean>(false);
-
     const handleOpenModal = () => setShowModal(true);
     const handleCloseModal = () => setShowModal(false);
 
-    return (<>
-        <MediaQuery minWidth={queries.minWidth}>
+    if (deviceType !== 'mobile') {
+        return (<>
             <div className={styles['video-list']}>
                 <Carousel
                     responsive={carouselQueries}
@@ -54,32 +50,33 @@ export default function VideoList({
                     ))}
                 </Carousel>
             </div>
-        </MediaQuery>
-        <MediaQuery maxWidth={queries.maxWidth}>
+        </>);
+    }
+
+    return (<>
+        <div className={styles['button-list-container']}>
+            <button className={styles['button-list']} onClick={handleOpenModal}>
+                Afficher les vidéos
+            </button>
+        </div>
+        {showModal && (<div className={styles['modal']}>
             <div className={styles['button-list-container']}>
-                <button className={styles['button-list']} onClick={handleOpenModal}>
-                    Afficher les vidéos
+                <button className={styles['button-list']} onClick={handleCloseModal}>
+                    Fermer les vidéos
                 </button>
             </div>
-            {showModal && (<div className={styles['modal']}>
-                <div className={styles['button-list-container']}>
-                    <button className={styles['button-list']} onClick={handleCloseModal}>
-                        Fermer les vidéos
-                    </button>
-                </div>
-                <div className={styles['video-list-mobile']}>
-                    {videos.map((video, key) => (
-                        <VideoItem
-                            video={video}
-                            videos={videos}
-                            currentVideo={currentVideo}
-                            handleChangeVideo={handleChangeVideo}
-                            onClick={handleCloseModal}
-                            key={key}
-                        />
-                    ))}
-                </div>
-            </div>)}
-        </MediaQuery>
+            <div className={styles['video-list-mobile']}>
+                {videos.map((video, key) => (
+                    <VideoItem
+                        video={video}
+                        videos={videos}
+                        currentVideo={currentVideo}
+                        handleChangeVideo={handleChangeVideo}
+                        onClick={handleCloseModal}
+                        key={key}
+                    />
+                ))}
+            </div>
+        </div>)}
     </>);
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,8 @@ export default function Document() {
 	return (
 		<Html lang='fr'>
 			<Head>
-				<link
-					href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=optional'
-					rel='stylesheet'
-				/>
+				<link rel='preconnect' href='https://fonts.googleapis.com' />
+				<link href='https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap' rel='stylesheet' />
 			</Head>
 			<body>
 				<noscript>
