f916c2b0d929d02a4bc71e9d78ac250685286c9b
Sonny <sonnyasdev@gmail.com>
2022-05-31 02:57:51 +0200

    add responsive home img header + parsing liens next

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,8 +25,31 @@ export default function Home() {
 
     return (<>
         <div className={styles['Home']}>
-            {/* @ts-ignore */}
-            <ReactMarkdown rehypePlugins={[rehypeRaw]} linkTarget='_blank' className={styles['markdown-gh']}>
+            <ReactMarkdown
+                // @ts-ignore
+                rehypePlugins={[rehypeRaw]}
+                linkTarget='_blank'
+                components={{
+                    a({ href, children }) {
+                        const url = 'https://installerwindows.fr';
+                        if (!href.startsWith(url)) {
+                            return (<>
+                                <a href={href} target='_blank' rel='noreferrer'>
+                                    {children}
+                                </a>
+                            </>);
+                        } else {
+                            const path = href.split(url)[1];
+                            return (<>
+                                <Link href={path}>
+                                    <a>{children}</a>
+                                </Link>
+                            </>);
+                        }
+                    }
+                }}
+                className={styles['markdown-gh']}
+            >
                 {markdown}
             </ReactMarkdown>
         </div>
