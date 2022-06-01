3dcc5ddc95d2a7d5baefa71e359f92b2fe721c08
Sonny <sonny.lepapa@gmail.com>
2022-06-02 00:20:55 +0200

    rework need help button

--- Components/NeedHelp/NeedHelp.tsx
+++ Components/NeedHelp/NeedHelp.tsx
@@ -5,14 +5,8 @@ import styles from './needhelp.module.scss';
 
 export default function NeedHelp() {
     return <>
-        <a href='https://discord.gg/informatique' rel='noreferrer' className={styles['ineedhelp']} target='_blank'>
-            <svg width='165px' height='50px' viewBox='0 0 165 50' className={styles['border']}>
-                <polyline points='164,1 164,49 1,49 1,1 164,1' className={styles['bg-line']} />
-                <polyline points='164,1 164,49 1,49 1,1 164,1' className={styles['hl-line']} />
-            </svg>
-            <div className={styles['text']}>
-                <FaDiscord /> Besoin d'aide ?
-            </div>
+        <a href='https://discord.gg/informatique' rel='noreferrer' className={`reset ${styles['ineedhelp']}`} target='_blank'>
+            <FaDiscord />
         </a>
     </>;
 }
