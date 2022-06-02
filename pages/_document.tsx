34382a814b6acfbc807e4e8be1d0bac21bcda976
Sonny <sonnyasdev@gmail.com>
2022-06-02 20:05:04 +0200

    ajout support pwa
    - Possibilité d'accéder au site sans internet (si les données ont été mises en cache côté client)
    - Possibilité "d'installer le site"

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,9 @@ export default function Document() {
 		<Html lang='fr'>
 			<Head>
 				<meta name='theme-color' content='#222831' />
+				<link rel='manifest' href='/manifest.json' />
+				<link rel='apple-touch-icon' href='/icon-192x192.png' />
+				<link rel='icon' href='/icon-192x192.png' />
 				<link rel='preconnect' href='https://fonts.googleapis.com' />
 				<link href='https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap' rel='stylesheet' />
 			</Head>
