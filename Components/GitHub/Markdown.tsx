37f8f7dddefce5705ad88e237a42878a1673b41b
Sonny <sonnyasdev@gmail.com>
2022-11-11 16:32:42 +0100

    add chapters

--- Components/GitHub/Markdown.tsx
+++ Components/GitHub/Markdown.tsx
@@ -2,8 +2,7 @@ import Image, { ImageProps } from "next/image";
 import Link from "next/link";
 import {
     AnchorHTMLAttributes,
-    ClassAttributes,
-    ImgHTMLAttributes
+    ClassAttributes, ImgHTMLAttributes, useEffect, useMemo, useRef, useState
 } from "react";
 
 import ReactMarkdown from "react-markdown";
@@ -14,25 +13,77 @@ const DOMAIN_URL = "https://installerwindows.fr";
 const YOUTUBE_DOMAIN = "https://www.youtube.com/";
 const YOUTUBE_SHORT_DOMAIN = "https://youtu.be/";
 
+interface Chapter {
+    name: string;
+    rect: DOMRect;
+}
+
 interface MarkdownProps {
     markdown: string;
     innerClassName?: string;
 }
 export default function Markdown({ markdown, innerClassName }: MarkdownProps) {
+    const listRef = useRef<HTMLUListElement>(null);
+
+    const scrollOffset = useMemo(() => {
+        const rect = listRef.current?.getBoundingClientRect();
+        return rect?.top ?? 0;
+    }, []);
+    const [chapters, setChapters] = useState<Chapter[]>([]);
+
+    const addChapter = (newChapter: Chapter) => {
+        setChapters((chapters) => {
+            const chapter = chapters.find((c => c.name === newChapter.name));
+            return chapter ? chapters : [...chapters, newChapter];
+        });
+    };
+
+    const handleScrollToElement = (chapter: Chapter) => {
+        window.scroll({
+            top: chapter.rect.top + scrollOffset,
+            behavior: 'smooth'
+        });
+    }
+
+    const TitleBuilder = ({ children, level }) => {
+        const ref = useRef<HTMLHeadingElement>();
+        const text = children?.[0] as string;
+
+        useEffect(() => {
+            if (ref?.current !== null) {
+                const rect = ref?.current?.getBoundingClientRect();
+                addChapter({ name: text, rect })
+            }
+        }, [children, ref, text]);
+
+        return level === 1 ?
+            <h1 ref={ref}>{text}</h1> :
+            <h2 ref={ref}>{text}</h2>;
+    }
+
     return (
-        <>
+        <div style={{ display: 'flex', gap: '15px' }}>
+            <ul className="chapters" style={{ height: 'fit-content', minWidth: '250px', position: 'sticky', top: '100px', marginTop: '50px' }} ref={listRef}>
+                {chapters.map((chapter, index) => (
+                    <li key={index + 1} onClick={() => handleScrollToElement(chapter)}>
+                        {index + 1}. {chapter.name}
+                    </li>
+                ))}
+            </ul>
             <ReactMarkdown
                 rehypePlugins={[rehypeRaw]}
                 linkTarget="_blank"
                 components={{
                     a: A_Builder,
                     img: IMG_Builder,
+                    h1: TitleBuilder,
+                    h2: TitleBuilder
                 }}
                 className={innerClassName}
             >
                 {markdown}
             </ReactMarkdown>
-        </>
+        </div>
     );
 }
 
@@ -40,13 +91,13 @@ type CustomAComponentsProps = ClassAttributes<HTMLAnchorElement> &
     AnchorHTMLAttributes<HTMLAnchorElement> &
     ReactMarkdownProps;
 
-const A_Builder = ({ href, children }: CustomAComponentsProps) => {
+function A_Builder({ href, children }: CustomAComponentsProps) {
     const path = getPathFromUrl(href, DOMAIN_URL);
     if (path) {
         return (
-            <>
-                <Link href={path}>{children}</Link>
-            </>
+            <Link href={path}>
+                {children}
+            </Link>
         );
     }
 
@@ -56,20 +107,18 @@ const A_Builder = ({ href, children }: CustomAComponentsProps) => {
     );
     if (videoId) {
         return (
-            <>
-                <Link href={`/videos/${videoId}`}>{children}</Link>
-            </>
+            <Link href={`/videos/${videoId}`}>
+                {children}
+            </Link>
         );
     }
 
     return (
-        <>
-            <Link href={href} target="_blank" rel="noreferrer">
-                {children}
-            </Link>
-        </>
+        <Link href={href} target="_blank" rel="noreferrer">
+            {children}
+        </Link>
     );
-};
+}
 
 function getPathFromUrl(url: string = null, domain: string = null) {
     if (!url || !domain) return url;
@@ -89,14 +138,14 @@ type CustomImgComponentsProps = ClassAttributes<HTMLImageElement> &
     ImgHTMLAttributes<HTMLImageElement> &
     ReactMarkdownProps;
 
-const IMG_Builder = ({
+function IMG_Builder({
     src,
     height,
     width,
     id,
     className,
     alt = "Logo",
-}: CustomImgComponentsProps) => {
+}: CustomImgComponentsProps) {
     let props = {
         height,
         width,
@@ -127,5 +176,5 @@ const IMG_Builder = ({
         };
     }
 
-    return <Image {...props} alt={props.alt} />;
-};
+    return (<Image {...props} alt={props.alt} />);
+}
