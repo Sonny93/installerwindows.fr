ac2cf9dd36e591b5785e3ebb9de3974605052a1b
Sonny <sonnyasdev@gmail.com>
2022-11-23 01:08:16 +0100

    improve chapters modal

--- Components/GitHub/Chapters.tsx
+++ Components/GitHub/Chapters.tsx
@@ -4,13 +4,17 @@ import { Chapter } from './Markdown';
 
 import styles from './markdown.module.scss';
 
-export default function Chapters({ chapters }: { chapters: Chapter[] }) {
+interface ChaptersProps {
+    chapters: Chapter[];
+    onSelect?: (event: any, chapter: Chapter) => void;
+}
+export default function Chapters({ chapters, onSelect }: ChaptersProps) {
     return (
         <ul className={styles['chapters']}>
             {chapters.map(({ id, name }, index) => (
                 <li key={index}>
-                    <Link href={`#${id}`} className="reset">
-                        {name} <span className={styles['number']}>- {index + 1}</span>
+                    <Link href={`#${id}`} className="reset" onClick={(event) => onSelect && onSelect(event, { id, name })}>
+                        {name}
                     </Link>
                 </li>
             ))}
---
+++ Components/Modal/Modal.tsx
@@ -0,0 +1,41 @@
+
+import { ReactElement } from "react";
+import ReactDOM from "react-dom";
+
+import styles from './modal.module.scss';
+
+interface ModalProps {
+    header: ReactElement<any> | string;
+    isShowing: boolean;
+    hide: () => void;
+    children: ReactElement<any> | string;
+}
+const Modal = ({ isShowing, hide, header, children }: ModalProps) =>
+    isShowing
+        ? ReactDOM.createPortal(
+            <>
+                <div className={styles["modal-overlay"]}>
+                    <div className={styles["modal-wrapper"]}>
+                        <div className={styles["modal"]}>
+                            <div className={styles["modal-header"]}>
+                                <h2>{header}</h2>
+                                <button
+                                    type="button"
+                                    className={styles["modal-close-button"]}
+                                    onClick={hide}
+                                >
+                                    <span>&times;</span>
+                                </button>
+                            </div>
+                            <div className={styles["modal-body"]}>
+                                {children}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>,
+            document.body
+        )
+        : null;
+
+export default Modal;
