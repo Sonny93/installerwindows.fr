3b02acbfb715d095f0772207902795f2808a7bb5
Sonny <sonnyasdev@gmail.com>
2023-03-16 21:07:24 +0100

    fix: cgu

--- pages/cgu.tsx
+++ pages/cgu.tsx
@@ -12,17 +12,32 @@ export async function getServerSideProps() {
     };
 }
 
-const markdown = `# Bienvenue sur notre site web
+const markdown = `# Conditions Générales d'Utilisation
 
-En utilisant ce site, vous acceptez les présentes conditions d'utilisation. Veuillez les lire attentivement avant d'utiliser ce site.
+Nous vous remercions de visiter notre site Web installerwindows.fr et nous vous prions de lire attentivement les conditions générales d'utilisation ci-dessous.
 
-Nous utilisons Hotjar pour améliorer la qualité de notre site web et comprendre comment les utilisateurs interagissent avec celui-ci. Hotjar est un service d'analyse de sites web qui utilise des cookies pour collecter des informations telles que les mouvements de souris, les clics et les actions des utilisateurs sur notre site. Ces informations sont utilisées pour améliorer la convivialité de notre site et l'expérience utilisateur. En utilisant notre site, vous acceptez que nous utilisions Hotjar à des fins statistiques.
+## Introduction
+Le site installerwindows.fr est destiné à fournir des informations utiles aux utilisateurs souhaitant installer ou réinstaller le système d'exploitation Windows sur leur ordinateur. En utilisant notre site Web, vous acceptez les présentes conditions générales d'utilisation.
 
 ## Cookies
+Notre site Web utilise des cookies pour améliorer votre expérience de navigation. Les cookies sont des fichiers texte stockés sur votre ordinateur ou votre appareil mobile lorsque vous accédez à un site Web. Les cookies nous aident à comprendre comment vous utilisez notre site Web et nous permettent d'améliorer votre expérience de navigation.
 
-Le site utilise des cookies pour collecter des informations sur l'utilisation du site par les utilisateurs. Voici une liste des cookies que nous utilisons et leurs fonctions :
+Nous utilisons également Hotjar, un outil d'analyse Web qui utilise des cookies pour collecter des informations sur la façon dont les utilisateurs naviguent sur notre site Web. Les cookies Hotjar collectent des informations telles que l'adresse IP de l'utilisateur, la taille de l'écran, le type d'appareil, le navigateur utilisé et les pages visitées. Ces informations sont utilisées pour améliorer l'expérience de l'utilisateur sur notre site Web.
 
--   Le cookie hj_truc est utilisé par Hotjar pour suivre les mouvements de souris et les clics des utilisateurs. Il nous permet de comprendre comment les utilisateurs naviguent sur notre site et d'identifier les problèmes éventuels.
--   Le cookie hj_blabla est utilisé par Hotjar pour enregistrer les sessions d'utilisateurs et les interactions avec le site. Cela nous permet de voir comment les utilisateurs interagissent avec notre site et d'identifier les domaines à améliorer.
+En utilisant notre site Web, vous consentez à l'utilisation des cookies et à la collecte d'informations par Hotjar.
 
-En utilisant notre site, vous acceptez l'utilisation de ces cookies à des fins statistiques et pour améliorer votre expérience utilisateur. Si vous ne souhaitez pas que nous utilisions des cookies, vous pouvez les désactiver dans les paramètres de votre navigateur. Veuillez noter que cela peut affecter la fonctionnalité de notre site.`;
+## Responsabilité
+Nous nous efforçons de fournir des informations exactes et à jour sur notre site Web. Toutefois, nous ne pouvons garantir l'exactitude, l'exhaustivité ou la pertinence des informations fournies. Nous ne sommes pas responsables des dommages directs ou indirects résultant de l'utilisation de notre site Web ou de l'impossibilité d'utiliser notre site Web.
+
+## Propriété intellectuelle
+Le contenu de notre site Web est protégé par les lois sur la propriété intellectuelle. Vous ne pouvez pas copier, modifier, distribuer ou utiliser le contenu de notre site Web à des fins commerciales sans notre autorisation écrite préalable.
+
+## Modification des présentes conditions générales
+Nous nous réservons le droit de modifier les présentes conditions générales d'utilisation à tout moment. Il est de votre responsabilité de vérifier régulièrement les conditions générales pour vous assurer que vous êtes au courant de tout changement.
+
+## Contactez-nous
+Si vous avez des questions ou des préoccupations concernant les présentes conditions générales d'utilisation ou l'utilisation des cookies sur notre site Web, veuillez nous contacter à l'adresse suivante : [adresse email].
+
+Cordialement,
+
+L'équipe d'installerwindows.fr`;
