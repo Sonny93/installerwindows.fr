b1febd4a0d4ececd43191501975f5e648c86d33c
Sonny <sonnyasdev@gmail.com>
2023-03-18 22:02:57 +0100

    feat: styling guides page

--- pages/guides/index.tsx
+++ pages/guides/index.tsx
@@ -5,6 +5,8 @@ import Footer from '../../Components/Footer/Footer';
 import Navbar from '../../Components/Navbar/Navbar';
 import { getGuides } from '../../lib/db';
 
+import styles from './guides.module.scss';
+
 interface PageGuidesProps {
     guides: Guide[];
 }
@@ -12,21 +14,42 @@ export default function PageGuides({ guides }: PageGuidesProps) {
     const session = useSession();
 
     return (
-        <div>
+        <div className={styles['guides']}>
             <Navbar />
-            <h1 style={{ textAlign: 'center' }}>Guides</h1>
-            <ul style={{ paddingLeft: '2em' }}>
-                {guides.length > 0 ? (
-                    guides.map(({ slug, title, isDraft }) => (
-                        <li key={slug} style={{ listStyle: 'disc', marginBottom: '15px' }}>
-                            {!isDraft && <Link href={`/guide/${slug}`}>{title}</Link>}
-                        </li>
-                    ))
-                ) : (
-                    <p>Aucun guide disponible</p>
-                )}
-            </ul>
-            {session.data && <Link href={'/guides/create'}>Ajouter un guide</Link>}
+            <main>
+                <h1 style={{ textAlign: 'center' }}>Guides</h1>
+                <ul style={{ paddingLeft: '2em', flex: '1' }}>
+                    {guides.length > 0 ? (
+                        guides.map(({ slug, title, isDraft }) => (
+                            <li key={slug} style={{ listStyle: 'disc', marginBottom: '15px' }}>
+                                {!isDraft && <Link href={`/guide/${slug}`}>{title}</Link>}
+                            </li>
+                        ))
+                    ) : (
+                        <p>Aucun guide disponible</p>
+                    )}
+                </ul>
+                {session.data && <Link href={'/guides/create'}>Ajouter un guide</Link>}
+                <p style={{ textAlign: 'center' }}>
+                    Vous souhaitez proposer un nouveau guide ou faire un retour ?<br />
+                    <Link
+                        href={'https://discord.com/invite/informatique'}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Rejoingez le serveur Discord
+                    </Link>{' '}
+                    et faites une proposition dans le salon{' '}
+                    <Link
+                        href="https://discord.com/channels/475253577288253440/1036644757524451478"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        idées-pour-le-serveur
+                    </Link>
+                    .
+                </p>
+            </main>
             <Footer />
         </div>
     );
