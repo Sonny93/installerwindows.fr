a62008162c5b3726467a48d18ab9fa80d1d91470
Sonny <sonnyasdev@gmail.com>
2023-03-21 10:15:20 +0100

    fix: video item thumbnail url

--- Components/VideoList/VideoItem.tsx
+++ Components/VideoList/VideoItem.tsx
@@ -3,29 +3,27 @@ import Link from 'next/link';
 
 import styles from './VideoList.module.scss';
 
-export default function VideoItem({
-    video,
-    currentVideo
-}: {
-    video: Video;
-    currentVideo: Video;
-}) {
+export default function VideoItem({ video, currentVideo }: { video: Video; currentVideo: Video }) {
     const { videoId, title } = video;
-    const miniatureUrl = `https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`;
+    const miniatureUrl = `https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`;
 
-    const className = `${styles['video-item']} ${currentVideo.videoId === videoId ? styles['selected'] : ''}`;
-    return (<>
-        <li className={className}>
-            <Link href={`/videos/${videoId}`} className='reset'>
-                <Image
-                    height={140}
-                    width={250}
-                    priority={true}
-                    src={miniatureUrl}
-                    alt='Miniature YTB'
-                />
-                <div className={styles['title']}>{title}</div>
-            </Link>
-        </li>
-    </>);
+    const className = `${styles['video-item']} ${
+        currentVideo.videoId === videoId ? styles['selected'] : ''
+    }`;
+    return (
+        <>
+            <li className={className}>
+                <Link href={`/videos/${videoId}`} className="reset">
+                    <Image
+                        height={140}
+                        width={250}
+                        priority={true}
+                        src={miniatureUrl}
+                        alt="Miniature YTB"
+                    />
+                    <div className={styles['title']}>{title}</div>
+                </Link>
+            </li>
+        </>
+    );
 }
