1755e9d4fb7e6396780173c79e50bdf4f6784841
Sonny <sonnyasdev@gmail.com>
2023-03-21 10:18:23 +0100

    refactor: public images folder

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-import { Head, Html, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
     return (
@@ -6,14 +6,15 @@ export default function Document() {
             <Head>
                 <meta name="theme-color" content="#222831" />
                 <link rel="manifest" href="/manifest.json" />
-                <link rel="apple-touch-icon" href="/icon-192x192.png" />
-                <link rel="icon" href="/icon-192x192.png" />
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" />
+                <link rel="apple-touch-icon" href="/images/icon-192x192.png" />
+                <link rel="icon" href="/images/icon-192x192.png" />
+                <link
+                    rel="stylesheet"
+                    href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
+                />
             </Head>
             <body>
-                <noscript>
-                    JavaScript recommandé pour une navigation optimale
-                </noscript>
+                <noscript>JavaScript recommandé pour une navigation optimale</noscript>
                 <Main />
                 <NextScript />
             </body>
