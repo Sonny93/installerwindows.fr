6426bfd67af09e5bcf86204193f3cf1b5d0fb4dc
Sonny <sonnyasdev@gmail.com>
2023-03-31 17:00:12 +0200

    chore: add docker & umami for analytic

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Head, Html, Main, NextScript } from 'next/document';
 
+const WEBSITE_ID = process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID;
+const SCRIPT_ORIGIN = process.env.NEXT_PUBLIC_UMAMI_SCRIPT_ORIGIN;
 export default function Document() {
     return (
         <Html lang="fr">
@@ -12,6 +14,14 @@ export default function Document() {
                     rel="stylesheet"
                     href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
                 />
+                {WEBSITE_ID && (
+                    <script
+                        async
+                        defer
+                        data-website-id={WEBSITE_ID}
+                        src={`${SCRIPT_ORIGIN}/umami.js`}
+                    />
+                )}
             </Head>
             <body>
                 <noscript>JavaScript recommandé pour une navigation optimale</noscript>
