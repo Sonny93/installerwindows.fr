c525489c8daf30d14eb4e4b4cc258b33c644ce3d
Sonny <sonnyasdev@gmail.com>
2025-01-08 18:26:17 +0100

    feat: add poppins font

--- inertia/layouts/base_layout.tsx
+++ inertia/layouts/base_layout.tsx
@@ -1,5 +1,5 @@
 import { primaryColor } from '#config/project';
-import { createTheme, MantineProvider } from '@mantine/core';
+import { createTheme, MantineProvider, rem } from '@mantine/core';
 import { PropsWithChildren } from 'react';
 
 const customTheme = createTheme({
@@ -18,6 +18,23 @@ const customTheme = createTheme({
 		],
 	},
 	primaryColor: 'blue',
+	fontFamily: 'Poppins, sans-serif',
+	respectReducedMotion: true,
+	headings: {
+		fontWeight: '400',
+
+		sizes: {
+			h1: {
+				fontSize: rem(36),
+				lineHeight: '1.4',
+			},
+			h2: { fontSize: rem(30), lineHeight: '1.5' },
+			h3: { fontSize: rem(24), lineHeight: '1.5' },
+			h4: { fontSize: rem(20), lineHeight: '1.5' },
+			h5: { fontSize: rem(16), lineHeight: '1.5' },
+			h6: { fontSize: rem(12), lineHeight: '1.5' },
+		},
+	},
 });
 
 export const BaseLayout = ({ children }: PropsWithChildren) => (
