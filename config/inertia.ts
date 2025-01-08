1c4a537769ffcb401d595be99abc9c1b8957a833
Sonny <sonnyasdev@gmail.com>
2025-01-08 01:11:47 +0100

    chore: init project

---
+++ config/inertia.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from '@adonisjs/inertia';
+import type { InferSharedProps, PageProps } from '@adonisjs/inertia/types';
+
+const inertiaConfig = defineConfig({
+	/**
+	 * Path to the Edge view that will be used as the root view for Inertia responses
+	 */
+	rootView: 'inertia_layout',
+
+	/**
+	 * Data that should be shared with all rendered pages
+	 */
+	sharedData: {
+		errors: (ctx) =>
+			ctx.inertia.always(() => ctx.session?.flashMessages.get('errors')),
+	},
+
+	/**
+	 * Options for the server-side rendering
+	 */
+	ssr: {
+		enabled: true,
+		entrypoint: 'inertia/app/ssr.tsx',
+	},
+});
+
+export default inertiaConfig;
+
+declare module '@adonisjs/inertia/types' {
+	export interface SharedProps
+		extends InferSharedProps<typeof inertiaConfig>,
+			PageProps {}
+}
