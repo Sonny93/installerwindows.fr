c1d7d99cb7d8ffa53190e3719876f2b669c6f603
Sonny <sonnyasdev@gmail.com>
2025-01-12 04:52:21 +0100

    fix: normalize title for slugify

--- shared/utils/index.ts
+++ shared/utils/index.ts
@@ -1,7 +1,8 @@
 export const slugify = (str: string = '') =>
 	str
+		.normalize('NFD')
+		.replace(/[\u0300-\u036f]/g, '')
 		.replace(/[^a-zA-Z0-9]/g, '-')
-		.replace(/-+/g, '-')
 		.toLowerCase();
 
 export const urlify = (text: string) => {
