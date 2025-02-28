468767b2079c58e74fbb6c158673e8eef1ba8c41
Sonny <sonnyasdev@gmail.com>
2025-02-28 01:20:52 +0100

    feat: improve guide list card style

--- inertia/pages/guides/index.tsx
+++ inertia/pages/guides/index.tsx
@@ -1,8 +1,8 @@
 import { discordSuggestionChannelUrl, helpUrl } from '#config/project';
 import type { Guides } from '#shared/types/index';
-import { Stack, Text, Title } from '@mantine/core';
+import { Link } from '@inertiajs/react';
+import { Button, Group, Stack, Text, Title } from '@mantine/core';
 import { ExternalLinkStyled } from '~/components/generics/links/external_link_styled';
-import { InternalLink } from '~/components/generics/links/internal_link';
 import { GuideList } from '~/components/guides/guide_list';
 import useUser from '~/hooks/use_user';
 
@@ -13,28 +13,36 @@ interface GuidesPageProps {
 export default function GuidesPage({ guides }: GuidesPageProps) {
 	const { isAuthenticated } = useUser();
 	return (
-		<Stack align="center" gap="xl">
-			<Title order={1}>Guides</Title>
+		<Stack gap="xl">
+			<Stack gap="xs">
+				<Group justify="space-between">
+					<Title order={1} style={{ lineHeight: 1.2 }}>
+						Guides
+					</Title>
+					{isAuthenticated && (
+						<Button component={Link} href="/guides/new">
+							Ajouter un guide
+						</Button>
+					)}
+				</Group>
+				<Text c="gray.6">
+					Vous souhaitez proposer un nouveau guide ou faire un retour ?<br />
+					<ExternalLinkStyled href={helpUrl} target="_blank" rel="noreferrer">
+						Rejoignez le serveur Discord
+					</ExternalLinkStyled>{' '}
+					et faites une proposition dans le salon{' '}
+					<ExternalLinkStyled
+						href={discordSuggestionChannelUrl}
+						target="_blank"
+						rel="noreferrer"
+					>
+						idées-pour-le-serveur
+					</ExternalLinkStyled>
+					.
+				</Text>
+			</Stack>
 			{guides.length === 0 && <Text>Aucun guide trouvé</Text>}
-			{isAuthenticated && (
-				<InternalLink href="/guides/new">Ajouter un guide</InternalLink>
-			)}
 			{guides.length > 0 && <GuideList guides={guides} />}
-			<Text style={{ textAlign: 'center' }}>
-				Vous souhaitez proposer un nouveau guide ou faire un retour ?<br />
-				<ExternalLinkStyled href={helpUrl} target="_blank" rel="noreferrer">
-					Rejoignez le serveur Discord
-				</ExternalLinkStyled>{' '}
-				et faites une proposition dans le salon{' '}
-				<ExternalLinkStyled
-					href={discordSuggestionChannelUrl}
-					target="_blank"
-					rel="noreferrer"
-				>
-					idées-pour-le-serveur
-				</ExternalLinkStyled>
-				.
-			</Text>
 		</Stack>
 	);
 }
