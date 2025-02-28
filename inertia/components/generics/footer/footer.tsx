e32cff742627bda68200a885be7dad6a548bdd1b
Sonny <sonnyasdev@gmail.com>
2025-02-28 03:08:54 +0100

    fix: add missing version and auth card in footer

--- inertia/components/generics/footer/footer.tsx
+++ inertia/components/generics/footer/footer.tsx
@@ -5,15 +5,26 @@ import {
 	editorYtbUrl,
 	projectGithubUrl,
 } from '#config/project';
+import * as packageJson from '@/package.json';
 import { Link } from '@inertiajs/react';
-import { Anchor, Box, Group, Stack, Text } from '@mantine/core';
+import {
+	ActionIcon,
+	Anchor,
+	Avatar,
+	Box,
+	Group,
+	Stack,
+	Text,
+} from '@mantine/core';
 import { ReactNode } from 'react';
 import { FaDiscord, FaGithub, FaYoutube } from 'react-icons/fa';
 import { IconType } from 'react-icons/lib';
 import { MdSignalWifiStatusbar4Bar } from 'react-icons/md';
-import { TbContract } from 'react-icons/tb';
+import { TbContract, TbLogin, TbLogout } from 'react-icons/tb';
 import { ExternalLinkStyled } from '~/components/generics/links/external_link_styled';
+import useUser from '~/hooks/use_user';
 import classes from './footer.module.css';
+
 interface FooterProps {
 	width: string;
 }
@@ -22,12 +33,17 @@ export const Footer = ({ width }: FooterProps) => (
 	<Box className={classes.footer}>
 		<Stack className={classes.footer__content} gap="xl" w={width}>
 			<Field label="Installerwindows.fr">
-				<Text className={classes.footer__description}>
-					Guides pour (ré)installer Windows 10/11 au propre et faire des
-					optimisations saines pour votre machine. Il s'agit de guides complets,
-					que vous devriez pouvoir suivre quel que soit votre niveau en
-					informatique.
-				</Text>
+				<Box>
+					<Text className={classes.footer__description}>
+						Guides pour (ré)installer Windows 10/11 au propre et faire des
+						optimisations saines pour votre machine. Il s'agit de guides
+						complets, que vous devriez pouvoir suivre quel que soit votre niveau
+						en informatique.
+					</Text>
+					<Text c="dimmed" size="sm" mt="xs">
+						v{packageJson.version}
+					</Text>
+				</Box>
 			</Field>
 			<Field label="Liens utiles">
 				<TextIcon icon={FaYoutube} href={editorYtbUrl} external>
@@ -43,8 +59,9 @@ export const Footer = ({ width }: FooterProps) => (
 					Statut des services
 				</TextIcon>
 				<TextIcon icon={TbContract} href="/cgu">
-					Conditions Générales d'Utilisations
+					CGUs
 				</TextIcon>
+				<UserCard />
 			</Field>
 			<Text className={classes.footer__credits}>
 				Réalisé avec ❤️ par{' '}
@@ -88,7 +105,11 @@ function TextIcon({
 
 	if (external) {
 		return (
-			<ExternalLinkStyled className={classes.footer__link} href={href}>
+			<ExternalLinkStyled
+				className={classes.footer__link}
+				href={href}
+				target="_self"
+			>
 				{content}
 			</ExternalLinkStyled>
 		);
@@ -97,5 +118,42 @@ function TextIcon({
 		<Anchor component={Link} className={classes.footer__link} href={href}>
 			{content}
 		</Anchor>
+	);
+}
+
+function UserCard() {
+	const auth = useUser();
+
+	if (auth.isAuthenticated) {
+		return <Authenticated />;
+	}
+
+	return <NotAuthenticated />;
+}
+
+const NotAuthenticated = () => (
+	<TextIcon icon={TbLogin} href="/auth/login" external>
+		Connexion
+	</TextIcon>
+);
+
+function Authenticated() {
+	const { user } = useUser();
+	return (
+		<Group gap="sm">
+			<Avatar size="sm" src={user!.avatarUrl} radius={30} />
+			<Text fz="sm" fw={500}>
+				{user!.fullname}
+			</Text>
+			<ActionIcon
+				size="sm"
+				component="a"
+				href="/auth/logout"
+				variant="filled"
+				color="red"
+			>
+				<TbLogout size={18} />
+			</ActionIcon>
+		</Group>
 	);
 }
