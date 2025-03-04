728e8a449b5639acebcb562a75d272d44c185630
Sonny <sonnyasdev@gmail.com>
2025-03-05 00:23:23 +0100

    refactor: log request middleware

--- app/middleware/log_request.ts
+++ app/middleware/log_request.ts
@@ -3,13 +3,15 @@ import logger from '@adonisjs/core/services/logger';
 
 export default class LogRequest {
 	async handle({ request }: HttpContext, next: () => Promise<void>) {
-		if (
-			!request.url().startsWith('/node_modules') &&
-			!request.url().startsWith('/inertia') &&
-			!request.url().startsWith('/@vite') &&
-			!request.url().startsWith('/@react-refresh') &&
-			!request.url().includes('.ts')
-		) {
+		const ignoreList = [
+			'/node_modules',
+			'/inertia',
+			'/@vite',
+			'/@react-refresh',
+			'.ts',
+			'package.json',
+		];
+		if (!ignoreList.some((ignore) => request.url().includes(ignore))) {
 			logger.debug(`[${request.method()}]: ${request.url()}`);
 		}
 		await next();
