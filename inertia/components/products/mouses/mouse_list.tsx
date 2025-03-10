edbe245133ffa84ec0646da70bc0c23ca248a78e
Sonny <sonnyasdev@gmail.com>
2025-03-10 02:26:22 +0100

    fix: product list wrap

--- inertia/components/products/mouses/mouse_list.tsx
+++ inertia/components/products/mouses/mouse_list.tsx
@@ -1,6 +1,6 @@
 import { Mouse } from '#shared/types/index';
 import { Link } from '@inertiajs/react';
-import { Button, SimpleGrid, Stack, Title } from '@mantine/core';
+import { Button, Flex, Stack, Title } from '@mantine/core';
 import { MouseCard } from '~/components/products/mouses/mouse_card';
 
 export function MouseList({ products }: { products: Mouse[] }) {
@@ -12,11 +12,11 @@ export function MouseList({ products }: { products: Mouse[] }) {
 			<Button component={Link} href="/periphs/create/mouse">
 				Ajouter une souris
 			</Button>
-			<SimpleGrid cols={3}>
+			<Flex gap="md" wrap="wrap">
 				{products.map((product) => (
 					<MouseCard key={product.id} product={product} />
 				))}
-			</SimpleGrid>
+			</Flex>
 		</Stack>
 	);
 }
