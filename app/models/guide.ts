0e19ee10a12ba198896b49f4b473a62747114305
Sonny <sonnyasdev@gmail.com>
2026-03-24 16:52:27 +0100

    refactor: models now use generated schemas

--- app/models/guide.ts
+++ app/models/guide.ts
@@ -1,16 +1,3 @@
-import AppBaseModel from '#models/app_base_model';
-import { column } from '@adonisjs/lucid/orm';
+import { GuideSchema } from '#database/schema';
 
-export default class Guide extends AppBaseModel {
-	@column()
-	declare title: string;
-
-	@column()
-	declare slug: string;
-
-	@column()
-	declare thumbnail: string;
-
-	@column()
-	declare githubRawUrl: string;
-}
+export default class Guide extends GuideSchema {}
--- app/models/user.ts
+++ app/models/user.ts
@@ -1,26 +1,7 @@
-import AppBaseModel from '#models/app_base_model';
-import type { DiscordToken } from '@adonisjs/ally/types';
-import { column, computed } from '@adonisjs/lucid/orm';
+import { UserSchema } from '#database/schema';
+import { computed } from '@adonisjs/lucid/orm';
 
-export default class User extends AppBaseModel {
-	@column({ serializeAs: null })
-	declare name: string;
-
-	@column({ serializeAs: null })
-	declare nickName: string;
-
-	@column()
-	declare avatarUrl: string;
-
-	@column({ serializeAs: null })
-	declare token?: DiscordToken;
-
-	@column({ serializeAs: null })
-	declare providerId: number;
-
-	@column({ serializeAs: null })
-	declare providerType: 'discord';
-
+export default class User extends UserSchema {
 	@computed()
 	get fullname() {
 		return this.nickName || this.name;
